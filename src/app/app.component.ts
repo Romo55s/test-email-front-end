72f166063f2b9c139aa33427dcd0e2f96d739a79
Tony <pp.tony777@gmail.com>
2023-05-25 02:49:55 -0600

    Succes on showing data from the back-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,22 @@ import * as Notiflix from 'notiflix';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Test-Email-front-end';
-
+  title = 'Test-Email;-front-end';
+  receiveDatadMsg: any = {};
+  okBack: boolean = false;
+  showReceivedData: boolean = false;
   data: FormGroup;
   // Inyectamos HttpClient para establecer la conexion con el back-end
-  constructor(private httpClient: HttpClient){
+  constructor(private httpClient: HttpClient) {
     // Incializamos el FormGroup
     this.data = new FormGroup({
-      email: new FormControl('',[Validators.required,Validators.email]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       about: new FormControl('', Validators.required),
-      mssg: new FormControl('',Validators.required)
+      mssg: new FormControl('', Validators.required)
     })
   }
 
-  sendEmail(){
+  sendEmail() {
     Notiflix.Loading.circle('Loading...');
     // Recuperamos los datos del form
     let params = {
@@ -31,13 +33,34 @@ export class AppComponent {
       mssg: this.data.value.mssg
     }
 
-    // Establecemos la conexion con el back-end
+    // Establecemos la conexión con el backend
     console.log(params);
-    this.httpClient.post('https://test-email-api.onrender.com/send',params).subscribe(res => {
+    this.httpClient.post('http://localhost:3000/send', params).subscribe((res: any) => {
       console.log(res);
       Notiflix.Loading.remove();
-      Notiflix.Notify.success('Successfully sent');
       this.data.reset();
+      if (res.ok) {
+        Notiflix.Notify.success('Successfully sent');
+        this.okBack = res.ok;
+        this.receiveDatadMsg = JSON.parse(res.msg);  // Guardamos los datos recibidos en `receivedData`
+        console.log(this.receiveDatadMsg);
+        // Puedes realizar cualquier acción adicional con los datos recibidos, como mostrarlos en el frontend
+      } else {
+        // La solicitud no fue exitosa, puedes realizar acciones específicas para manejar errores
+        this.okBack = res.ok;
+        Notiflix.Notify.failure('Something went wrong. Try again...');
+        console.log('Error en la respuesta del servidor');
+      }
     })
   }
+
+  status() {
+    if(this.okBack ){
+      this.showReceivedData = true;
+    }else{
+      Notiflix.Notify.failure('No data available.');
+    }
+    
+  }
+
 }
